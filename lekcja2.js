01f7b7f3595413a8b7d9535ee3b7f770029c284c
Laborabis <adam.laborabis@gmail.com>
2024-02-18 12:38:48 +0100

    Instrukcje warunkowe

---
+++ lekcja2.js
@@ -0,0 +1,10 @@
+const login = "Tomek1";
+const password = "supervisor"
+
+if (login === "Tomek" && password === "password" || password ==="supervisor") {
+    alert("Udało się zalogować")    
+} 
+
+else {
+    alert("Nie udało się zalogować !!!!!!!!!!")
+}
