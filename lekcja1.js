88f809bd3ff82772ac0105444473d446d87ccc48
Laborabis <adam.laborabis@gmail.com>
2024-02-18 11:51:26 +0100

    Podstawowe zmienne

--- lekcja1.js
+++ lekcja1.js
@@ -1,2 +1,16 @@
-const zmennaStala = "Tomasz";
-let  
+let zmiennaNumber = 20.00;
+let zmiennaString = "test";
+let zmiennaBoolean = false;
+let zmiennaUndefined = undefined;
+let zmiennaArray = ["a", 12, "bb", 22.2, false]
+let zmiennaObject = {
+    color: "blue",
+    quantity: 7,
+    isTrue: true
+}
+
+let wynik = zmiennaNumber + zmiennaString + zmiennaBoolean + zmiennaUndefined + zmiennaArray[3] + zmiennaObject.color;
+
+console.log("Wynik: " + wynik);
+console.log("Wynik: " + wynik.length);
+console.log("Wynik: " + wynik.slice(7,20));
